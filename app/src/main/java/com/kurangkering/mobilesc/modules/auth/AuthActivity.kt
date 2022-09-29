0e6bca4782c9e01ccae0e80b5a99851a2139846a
KurangKering <cybercature@gmail.com>
2022-09-29 20:15:42 +0700

    binding view with material ui

--- app/src/main/java/com/kurangkering/mobilesc/modules/auth/AuthActivity.kt
+++ app/src/main/java/com/kurangkering/mobilesc/modules/auth/AuthActivity.kt
@@ -1,12 +1,12 @@
-package com.kurangkering.mobilesc
+package com.kurangkering.mobilesc.modules.auth
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.kurangkering.mobilesc.R
 
-class MainActivity : AppCompatActivity() {
-
+class AuthActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_auth)
     }
 }
